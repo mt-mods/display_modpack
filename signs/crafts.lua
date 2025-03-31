ef89d6c62edd9cb0ddf5f3e885a7e7384d5d4381
whosit <knbdevel@gmail.com>
2025-03-31 15:41:57 +0300

    remove xcompat check

--- signs/crafts.lua
+++ signs/crafts.lua
@@ -48,25 +48,22 @@ minetest.register_craft({
 	}
 })
 
-if xcompat then
-	local m = xcompat.materials
-	local dyes = {
-		m.dye_white,
-		m.dye_grey,
-		m.dye_orange,
-		m.dye_brown,
-		m.dye_dark_grey,
-		m.dye_yellow,
-		m.dye_green,
-		m.dye_red,
-	}
-	for i, dye in ipairs(dyes) do
-		minetest.register_craft({
-			type = "shapeless",
-			output = minetest.itemstring_with_palette("signs:paper_poster", (i-1) * 32),
-			recipe = {"signs:paper_poster", dye},
-		})
-	end
+local dyes = {
+	mat.dye_white,
+	mat.dye_grey,
+	mat.dye_orange,
+	mat.dye_brown,
+	mat.dye_dark_grey,
+	mat.dye_yellow,
+	mat.dye_green,
+	mat.dye_red,
+}
+for i, dye in ipairs(dyes) do
+	minetest.register_craft({
+		type = "shapeless",
+		output = minetest.itemstring_with_palette("signs:paper_poster", (i-1) * 32),
+		recipe = {"signs:paper_poster", dye},
+	})
 end
 
 minetest.register_craft({
